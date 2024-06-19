0d6567edb7ce3499b967dc30116aca302a39ae8b
Tntragan <tomandtammyragan@gmail.com>
2024-06-19 15:58:53 -0700

    Initial commit

--- index.js
+++ index.js
@@ -2,12 +2,30 @@ const express = require('express')
 const app = express()
 const cors = require('cors')
 require('dotenv').config()
+const mongoose = require('mongoose');
+
+mongoose.connect(process.env.MONGO_URI);
+
+const peopleSchema = new mongoose.Schema({
+  username: {
+    type: String,
+    required: true
+  },
+  _id: String
+});
+
+const Person = mongoose.model('Person', peopleSchema);
 
 app.use(cors())
 app.use(express.static('public'))
+
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/views/index.html')
 });
+
+app.post('/api/users', (req, res) => {
+
+})
 
 
 
